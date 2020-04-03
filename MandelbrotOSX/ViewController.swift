c6adaf271e85c68e9c5b28439adaf4f973c031a8
guitarmike1966 <guitarmike1966@gmail.com>
2020-04-03 13:40:10 -0500

    Initial Commit

---
+++ MandelbrotOSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MandelbrotOSX
+//
+//  Created by mikey on 4/3/20.
+//  Copyright © 2020 Sand Creek Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
